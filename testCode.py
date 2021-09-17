ac360d6b660e3dcd00c1fad95444802bd6878ed5
Shray Swarup <shrayswarup@gmail.com>
2021-09-16 21:06:51 -0700

    Initial

---
+++ testCode.py
@@ -0,0 +1,10 @@
+def convert(lst):
+    return (lst[0].split())
+ 
+# Driver code
+lst =  ["Abc def ghi"]
+print( convert(lst))
+
+my_dict = {'a': 'hello', 'b': 'world'}
+for key, value in my_dict.items():
+    print(key)
