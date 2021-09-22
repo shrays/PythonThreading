52bea869a7a40300a96605160312c89e3f22aff2
Shray Swarup <shrayswarup@gmail.com>
2021-09-21 21:49:09 -0700

    edits

--- singleThread.py
+++ singleThread.py
@@ -5,8 +5,8 @@ words = [open('input.txt', 'r').read()]
 words = words[0].split()
 #print(words)
 
-start_time = time.time()
-for x in words:
+start_time = time.time() # starts time clock
+for x in words: # counts word(key) occurences(value) in list to dictionary
     if x in data:
         data[x] += 1
     else:
